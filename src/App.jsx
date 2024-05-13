e36ecffdbe0d4071bdfe5bf756a44db075258e09
Nikhilesh1119 <nikhileshchouhan20685@acropolis.in>
2024-05-13 15:43:25 +0530

    Teacherpage added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 import TeacherPage from "./components/TeacherPage";
+
 // import StudentPage from './components/StudentPage';
 // import ClassroomPage from './components/ClassroomPage';
 // import Update from './components/Update';
 // import Profile from './components/Profile';
 // import LoginSignup from './components/LoginSignup';
-// import Home from './components/Home';
 
 function App() {
   return (
@@ -14,13 +15,15 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/teacher" element={<TeacherPage />} />
-          {/* <Route path = "/" element = {<Home />} />
+          {/* 
           <Route path="/student" element={<StudentPage />} />
           <Route path="/classroom" element={<ClassroomPage />} />
           <Route path="/Updates" element={<Update />} />
           <Route path="/Profile" element={<Profile/>} /> 
-          <Route path="/LoginPage" element={<LoginSignup/>} /> */}
+          <Route path="/LoginPage" element={<LoginSignup/>} />
+           */}
         </Routes>
       </Router>
     </div>
