1ae474e32fbc09f2540391d9ff71d999fa307e89
jainam singhai <jainamsinghai20460@acropolis.in>
2024-05-13 16:23:53 +0530

    event added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import TeacherPage from "./components/TeacherPage";
+import Events from "./components/Event";
+import GuestLogin from "./pages/Login";
 // import StudentPage from './components/StudentPage';
 // import ClassroomPage from './components/ClassroomPage';
 // import Update from './components/Update';
@@ -15,6 +17,8 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/teacher" element={<TeacherPage />} />
+          <Route path="/event" element={<Events />} />
+         
           {/* <Route path = "/" element = {<Home />} />
           <Route path="/student" element={<StudentPage />} />
           <Route path="/classroom" element={<ClassroomPage />} />
