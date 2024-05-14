8f9912af54b72f41e44a3dd546c62c43213b7d1d
Nikhilesh1119 <nikhileshchouhan20685@acropolis.in>
2024-05-14 13:19:54 +0530

    Classroom added

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import TeacherPage from "./components/TeacherPage";
 import StudentPage from "./components/StudentPage";
+import ClassSetup from "./components/ClassRoom";
 
-// import ClassroomPage from './components/ClassroomPage';
 // import Update from './components/Update';
 // import Profile from './components/Profile';
 // import LoginSignup from './components/LoginSignup';
@@ -18,8 +18,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/teacher" element={<TeacherPage />} />
           <Route path="/student" element={<StudentPage />} />
+          <Route path="/classroom" element={<ClassSetup />} />
+
           {/* 
-          <Route path="/classroom" element={<ClassroomPage />} />
           <Route path="/Updates" element={<Update />} />
           <Route path="/Profile" element={<Profile/>} /> 
           <Route path="/LoginPage" element={<LoginSignup/>} />
